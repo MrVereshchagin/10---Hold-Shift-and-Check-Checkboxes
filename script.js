a681d144b0a39c15f82285efa821c0f589bf9b13
Igor Vereshchagin <9868425@gmail.com>
2021-06-07 13:16:29 +0300

    feat: add some comments for me

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
-const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]'); // формируем коллекцию узлов NodeList, обращение все инпуты с типом checkbox внутри элементов с классом .inbox, запись через пробел
 console.log(checkboxes);
 
 let lastChecked;
 
 function handleCheck(e) {
     let inBetween = false;
+    // проверяем ниже два события: нажата ли клавиша shift - относится к событиям мыши && включен ли флажок у элемента (есть ли атрибут cheked) передаваемого в данную функцию 
     if (e.shiftKey && this.checked) {
-        checkboxes.forEach(checkbox => {
+        checkboxes.forEach(checkbox => { //перебираем коллекцию с колбэком: если 
             console.log(checkbox);
-            if (checkbox === this || checkbox === lastChecked) {
-                inBetween = !inBetween;
-                console.log('Start check them in between');
+            if (checkbox === this || checkbox === lastChecked) { // элемент чекбокса который выбран является первым или чекбокс который выбран является последним из выбранных
+                inBetween = !inBetween; // меняется на true
+                console.log('Start check them in between'); // выводим в консоль надпись какие элементы должны быть выбраны между указанными элементами
             }
 
-            if (inBetween) {
-                checkbox.checked = true;
+            if (inBetween) { // и если inBetween true
+                checkbox.checked = true; // присваиваем указанным эелемнтам атрибут checked
             }
         });
     }
-    lastChecked = this;
+    lastChecked = this; // привязываем переменную к конексту вызова функции 
 }
 
 checkboxes.forEach(checkbox =>checkbox.addEventListener('click', handleCheck));
